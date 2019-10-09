585bcf22ccc94d579d7e392ed1408786e87dcc84
Alessandro dos Santos <santos.alessandro1990@hotmail.com>
2019-10-09 20:48:12 -0300

    Arrumando as validações do cadastro

--- src/app/cadastros/unidades/cadastro/cadastro.component.ts
+++ src/app/cadastros/unidades/cadastro/cadastro.component.ts
@@ -18,12 +18,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent implements OnInit {
-  public loading : boolean;
-  public error : boolean;
-  public sucess : boolean;
-  public isContainer : boolean;
-  public isBl : boolean;
-  public blIdString : string;
+  public loading: boolean;
+  public error: boolean;
+  public sucess: boolean;
+  public isContainer: boolean;
+  public isBl: boolean;
+  public blIdString: string;
 
   public mensagem = '';
   public lstLivros: Livros[] = [];
@@ -38,8 +38,8 @@ export class CadastroComponent implements OnInit {
   public container: Container = new Container();
   public bl: Bl = new Bl();
 
-  public isNew : boolean;
-  public title : string;
+  public isNew: boolean;
+  public title: string;
 
   constructor(
     private autorService: AutorService,
@@ -65,74 +65,66 @@ export class CadastroComponent implements OnInit {
     this.router.paramMap.subscribe(c => {
       if (c.get('local') != null) {
         this.title = c.get('local');
-        if(c.get('local') == 'container')
-        {
+        if (c.get('local') == 'container') {
           this.isContainer = true;
         }
-        else
-        {
+        else {
           this.isBl = true;
         }
       }
-      else
-      {
+      else {
         this.isNew = true;
-      }   
+      }
     });
 
-    if(this.isContainer)
-    {
-      this.blService.getAll().toPromise().then(lbl =>{
+    if (this.isContainer) {
+      this.blService.getAll().toPromise().then(lbl => {
         this.lstBls = lbl;
       });
     }
 
-    if(this.isContainer )
-    {
+    if (this.isContainer) {
       this.router.paramMap.subscribe(c => {
         if (c.get('id') != null) {
           this.containerService.getAll(Number(c.get('id'))).toPromise().then(x => {
             this.loading = false;
-            this.container = x[0];         
+            this.container = x[0];
             this.isNew = false;
           })
-          .catch( e=> {
-            this.error = true;
-            this.mensagem = 'Houve um erro ao buscar informações do Container'
-          })
-          .finally(() => {this.loading = false;});
+            .catch(e => {
+              this.error = true;
+              this.mensagem = 'Houve um erro ao buscar informações do Container'
+            })
+            .finally(() => { this.loading = false; });
         }
-        else
-        {
+        else {
           this.loading = false;
           this.isNew = true;
-        }   
+        }
       })
-      ;
+        ;
     }
-    else
-    {
+    else {
       this.router.paramMap.subscribe(c => {
         if (c.get('id') != null) {
           this.blService.getAll(Number(c.get('id'))).toPromise().then(x => {
             this.loading = false;
-            this.bl = x[0];         
+            this.bl = x[0];
             this.isNew = false;
           })
-          .catch( e=> {
-            this.error = true;
-            this.mensagem = 'Houve um erro ao buscar informações do Container'
-          });
+            .catch(e => {
+              this.error = true;
+              this.mensagem = 'Houve um erro ao buscar informações do Container'
+            });
         }
-        else
-        {
+        else {
           this.loading = false;
           this.isNew = true;
-        }   
+        }
       });
     }
 
-    
+
     this.loading = true;
     this.autor = new Autores();
     this.livro = new Livros();
@@ -145,8 +137,7 @@ export class CadastroComponent implements OnInit {
     this.loading = false;
   }//ON INIT
 
-  novoContainer()
-  {
+  novoContainer() {
     this.container.nomeConsignatario = '';
     this.container.nomeNavio = '';
     this.container.numero = null;
@@ -154,64 +145,138 @@ export class CadastroComponent implements OnInit {
     this.container.tipo = '';
   }
 
-  novoBl()
-  {
+  novoBl() {
     this.bl.consignee = '';
     this.bl.navio = '';
     this.bl.numero = null;
   }
 
-  cadastrarContainer()
-  {
-    this.sucess = false;
-    this.error = false;
+  validatorContainer() {
+    debugger;
+    let errorValidation: boolean = false;
+    this.mensagem = '';
 
-    this.mensagem = "";
-    if (this.isNew) 
-    {
-      debugger;
-      this.containerService.insert(this.container).toPromise().then(c => {
-        this.sucess = true;
-        this.mensagem = ('Registro inserido com sucesso.');
-      });
-    } else {
-      debugger;
-      this.containerService.edit(this.container).toPromise().then(c => {
-        debugger;
-        this.sucess = true;
-        this.mensagem = ('Registro atualizado com sucesso.');
-      })
-      .catch( e => {
-        debugger;
-        this.error = true;
-        this.mensagem = 'Houve um erro ao inserir o registro.';
-      })
-      .finally(()=> {this.loading = false;});
+    if (this.container.numero === 0) {
+      errorValidation = true;
+      this.mensagem += 'O número do container deve ser maior que 0 \n';
+    }
+    if (this.container.numero === undefined || this.container.numero === null) {
+      errorValidation = true;
+      this.mensagem += 'O número do container é obrigatório \n';
+    }
+    if (this.container.tamanho === 0) {
+      errorValidation = true;
+      this.mensagem += 'O tamanho do container deve ser maior que 0 \n';
+    }
+    if (this.container.tamanho === null || this.container.tamanho === undefined) {
+      errorValidation = true;
+      this.mensagem += 'O tamanho do container é obrigatório \n';
+    }
+    if (this.container.tipo === '' || this.container.tipo === undefined || this.container.tipo === null) {
+      errorValidation = true;
+      this.mensagem += 'É obrigatório informar o tipo do container \n';
+    }
+
+    if (errorValidation === true) {
+      return true;
+    }
+    else {
+      return false;
     }
   }
 
-  cadastrarBl()
-  {
+  validatorBl() {
+    let errorValidation: boolean = false;
+    this.mensagem = '';
+
+    if (this.bl.numero === 0) {
+      errorValidation = true;
+      this.mensagem += 'O número do BL deve ser maior que 0 \n';
+    }
+    if (this.bl.numero === null || this.bl.numero === undefined) {
+      errorValidation = true;
+      this.mensagem += 'O número do BL é obrigatorio \n';
+    }
+    if (this.bl.consignee === '' || this.bl.consignee === null || this.bl.consignee === undefined) {
+      errorValidation = true;
+      this.mensagem += 'O Consignatário do BL é um campo obrigatório \n';
+    }
+    if (this.bl.navio === '' || this.bl.navio === undefined || this.bl.navio == null) {
+      errorValidation = true;
+      this.mensagem += 'O Navio é um campo obrigatório \n';
+    }
+
+    if (errorValidation === true) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  cadastrarContainer() {
     this.sucess = false;
     this.error = false;
 
     this.mensagem = "";
-    if (this.isNew) 
+    debugger;
+    if (!this.validatorContainer()) {
+      if (this.isNew) {
+        debugger;
+        this.containerService.insert(this.container).toPromise().then(c => {
+          this.sucess = true;
+          this.mensagem = ('Registro inserido com sucesso. \n');
+        });
+      } else {
+        debugger;
+        this.containerService.edit(this.container).toPromise().then(c => {
+          debugger;
+          this.sucess = true;
+          this.mensagem = ('Registro atualizado com sucesso. \n');
+        })
+          .catch(e => {
+            debugger;
+            this.error = true;
+            this.mensagem += 'Houve um erro ao inserir o registro. \n';
+          })
+          .finally(() => { this.loading = false; });
+      }
+    }
+    else {
+      debugger;
+      this.error = true;
+      this.mensagem += 'Houve um erro ao inserir o registro. \n';
+    }
+  }
+
+  cadastrarBl() {
+    this.sucess = false;
+    this.error = false;
+
+    this.mensagem = "";
+    debugger;
+    if (!this.validatorBl())
     {
-      this.blService.insert(this.bl).toPromise().then(c => {
-        this.sucess = true;
-        this.mensagem = ('Registro inserido com sucesso.');
-      });
-    } else {
-      this.blService.edit(this.bl).toPromise().then(c => {
-        this.sucess = true;
-        this.mensagem = ('Registro atualizado com sucesso.');
-      })
-      .catch( e => {
-        this.error = true;
-        this.mensagem = 'Houve um erro ao inserir o registro.';
-      })
-      .finally(()=> {this.loading = false;});
+      if (this.isNew) {
+        this.blService.insert(this.bl).toPromise().then(c => {
+          this.sucess = true;
+          this.mensagem = ('Registro inserido com sucesso. \n');
+        });
+      } else {
+        this.blService.edit(this.bl).toPromise().then(c => {
+          this.sucess = true;
+          this.mensagem = ('Registro atualizado com sucesso. \n');
+        })
+          .catch(e => {
+            this.error = true;
+            this.mensagem += 'Houve um erro ao inserir o registro. \n';
+          })
+          .finally(() => { this.loading = false; });
+      }
+    }
+    else {
+      this.error = true;
+      this.mensagem += 'Houve um erro ao inserir o registro. \n';
     }
   }
 }//CLASS
